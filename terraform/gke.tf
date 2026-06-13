85d7973727bc4c914a62c6883b8d216814c55728
ShaungGautham <thurazaw.gtm@gmail.com>
2026-06-13 21:17:02 +0700

    fix: syntax error in gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  network    = google_compute_network.vpc.name
+  network    = google_compute_network.vpc_network.name
 
   remove_default_node_pool = true
   initial_node_count       = 1
